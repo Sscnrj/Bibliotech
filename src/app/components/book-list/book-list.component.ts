76a0ddedc5e00336a3af5490ec5ed17ed1b12e9b
Sscnrj <remi.jegard@gmail.com>
2025-04-24 18:13:16 +0200

    Jusqu'a todo 16

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { BookService } from '../../services/book.service';
 import { Book } from '../../models/book.model';
 import { Router } from '@angular/router';
+import {HighlightDirective} from '../../directives/highlight.directive';
+import {TruncatePipe} from '../../pipes/truncate.pipe';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [CommonModule, RouterLink, FormsModule, HighlightDirective, TruncatePipe],
   templateUrl: './book-list.component.html',
 })
 export class BookListComponent implements OnInit {
@@ -21,11 +23,11 @@ export class BookListComponent implements OnInit {
     private bookService: BookService,
     private router: Router
   ) {}
-  
+
   ngOnInit(): void {
     this.loadBooks();
   }
-  
+
   loadBooks(): void {
     this.bookService.getBooks().subscribe({
       next: (books: Book[]) => {
@@ -36,11 +38,11 @@ export class BookListComponent implements OnInit {
       }
     });
   }
-  
+
   toggleFavorite(book: Book): void {
     this.bookService.toggleFavorite(book.id).subscribe({
       next: (updatedBook: Book) => {
-        // TODO 16: Affiche une alerte qui indique que le favori a été modifié
+        alert(`Le livre "${updatedBook.title}" a été ${updatedBook.isFavorite ? 'ajouté aux' : 'retiré des'} favoris.`);
       },
       error: (err: any) => {
         // TODO 17: Affiche une alerte qui indique que la modification du favori a échoué
@@ -48,7 +50,7 @@ export class BookListComponent implements OnInit {
       }
     });
   }
-  
+
   deleteBook(id: string): void {
     this.bookService.deleteBook(id).subscribe({
       next: () => {
@@ -60,7 +62,7 @@ export class BookListComponent implements OnInit {
         console.error('Erreur lors de la suppression du livre:', err);
       }
     });
-  } 
+  }
 
   goToBookDetails(id: string): void {
     this.router.navigate(['/books', id]);
